16de2b5e2cebd52196af996646e8ada93f1ef149
makhijanaresh <makhijanaresh@hotmail.com>
2019-02-26 21:39:55 +0530

    First Commit

---
+++ src/test/java/com/mkyong/hashing/AppTest.java
@@ -0,0 +1,30 @@
+package com.mkyong.hashing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    
+	
+	@Test
+	public void samplePass()
+	{
+		int i=1;
+		int j=2;
+		assertTrue(i+j==3);
+	}
+	
+	@Test
+	public void sampleFail()
+	{
+		int i=1;
+		int j=2;
+		assertTrue(i+j==4);
+	}
+	
+	
+}
